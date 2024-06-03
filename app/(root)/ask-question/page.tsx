ec858d2b4a02ce4c3601100dd9748ee38c4ea49c
Manan dudeja <manandudeja6@gmail.com>
2024-06-03 17:50:51 +0530

    askQuestion changes

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,18 @@
+import Question from "@/components/forms/Question";
 import React from "react";
 
 const page = () => {
   return (
     <div>
-      <h1>Ask a Question</h1>
+      <h1
+        className="h1-bold 
+      text-dark100_light900"
+      >
+        Ask a Question
+      </h1>
+      <div className="mt-9">
+        <Question />
+      </div>
     </div>
   );
 };
