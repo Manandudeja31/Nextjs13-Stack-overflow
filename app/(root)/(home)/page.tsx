c5cbbf0e16dd25f71337e58062ba8d291c06217f
Manan dudeja <manandudeja6@gmail.com>
2024-06-11 01:57:06 +0530

    console log

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -11,8 +11,6 @@ import { getQuestions } from "@/lib/actions/question.action";
 export default async function Home() {
   const result = await getQuestions({});
 
-  console.log(result.questions);
-
   return (
     <>
       <div
