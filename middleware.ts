f300a9814b5ec94fc486a1260e14732a4d064c57
Manan dudeja <manandudeja6@gmail.com>
2024-06-11 18:59:38 +0530

    add api/webhook to public routes

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,27 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/ask-question"]);
-
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook",
+  "/questions/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+]);
+// const ignoredRoutes = createRouteMatcher(["/api/webhook", "/api/chatgpt"]);
 export default clerkMiddleware((auth, req) => {
-  if (!auth().userId && isProtectedRoute(req)) {
-    // Add custom logic to run before redirecting
+  if (isPublicRoute(req)) {
+    return;
+  }
 
+  if (!auth().userId && isProtectedRoute(req)) {
     return auth().redirectToSignIn();
   }
 });
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
