6c28f9d7dcd043ff534a002224d971641b4c499b
Manan dudeja <manandudeja6@gmail.com>
2024-07-11 03:25:30 +0530

    edit_delete_question&answer

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import RenderTag from "../shared/RenderTag";
 import Metric from "../shared/Metric";
 import { formatBigNumber, getTimestamp } from "@/lib/utils";
+import { SignedIn } from "@clerk/nextjs";
+import EditDeleteAction from "../shared/EditDeleteAction";
 interface QuestionProps {
   _id: string;
   title: string;
@@ -32,6 +34,7 @@ const QuestionCard = ({
   answers,
   createdAt,
 }: QuestionProps) => {
+  const showActionButtons = clerkId && clerkId === author.clerkId;
   return (
     <div
       className="card-wrapper rounded-[10px] 
@@ -60,7 +63,12 @@ const QuestionCard = ({
             </h3>
           </Link>
         </div>
-        {/* If signed in add edit delete actions */}
+
+        <SignedIn>
+          {showActionButtons && (
+            <EditDeleteAction type="Question" itemId={JSON.stringify(_id)} />
+          )}
+        </SignedIn>
       </div>
       <div className="mt-3.5 flex flex-wrap gap-2">
         {tags.map((tag) => (
